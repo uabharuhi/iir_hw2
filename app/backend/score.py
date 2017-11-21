b0946a81cfd309558961eda2e34b14c8f057b5d9
e17n2 <uabharuhiu@gmail.com>
2017-11-21 19:41:33 +0800

    tf idf

--- app/backend/score.py
+++ app/backend/score.py
@@ -3,6 +3,16 @@ from  collections import Counter
 
 
 
+
+#def make_tf_func(name):
+#    log_count = lambda cnt:1+math.log(cnt)
+#    
+#    if name=='l':
+
+
+
+
+
 ## input : 
 #   document -- list of tokens
 #   token2id -- key is token ,output is id
@@ -32,14 +42,33 @@ def idf_t(document_list,token2id):
         for docuemnt_token_list in document_list:
             if  token in  docuemnt_token_list:
                 cnt+=1
-        idf_list[ID] = math.log(len(token2id)/(cnt+1))
+        idf_list[ID] = math.log(len(document_list)/(cnt+1))
 
     return idf_list
 
 
 class ScoreAlgorithm():
-    def __init__(self,tf_func,idf_func):
-        pass
+    def __init__(self,token2id,tf_func=tf_logrithm,idf_func=idf_t):
+        self.token2id = token2id
+        self.tf_func = tf_func
+        self.idf_func = idf_func
+    ## input : 
+    #   document_list -- list of  document , each document is a list of token
+    #  output :
+    #   scores Llist of document vector,each document vector is a list of tf-idf value
+    #  
+    def calculate(self,document_list):
+        idf_list = self.idf_func(document_list, self.token2id)
+        scores = []
+        for document in document_list:
+            term_freq_list = self.tf_func(document, self.token2id)
+            for i,term_freq in enumerate(term_freq_list):
+                term_freq_list[i]*= idf_list[i]
+            scores.append(term_freq_list)
+        return scores
+
+
+
 
 
 
--- app/backend/test/testscore.py
+++ app/backend/test/testscore.py
@@ -1,10 +1,40 @@
-from backend.score import tf_logrithm
+from backend.score import tf_logrithm, idf_t,ScoreAlgorithm
 from backend.util import getLookupTables
 
 def test_tf():
-    vocab=['i','have','a','pen','.','i','have','a','dog','.']
+    print("term frequency")
+    vocab = list(set(['i','have','a','pen','.','i','have','an','apple','.','oh','!','apple','pen']))
     id2token,token2id  = getLookupTables(vocab)
-    print(tf_logrithm(vocab,token2id))
-    print(id2token)
-
+    print("i hava a pen.a")
+    print(tf_logrithm(['i','have','a','pen','.','a'],token2id))
+    print("i hava an apple.")
+    print(tf_logrithm(['i','have','an','apple','.'],token2id))
+    print("oh! apple pen")
+    print(tf_logrithm(['oh','!','apple','pen'],token2id))
+    print('- '*50)
+    #print(id2token)
+def test_idf():
+    print("inverse document frequency")
+    documents = [['i','have','a','pen','.'],
+                ['i','have','an','apple','.'],
+                ['i','oh','!','apple','pen']]
+    vocab = list(set(['i','have','a','pen','.','i','have','an','apple','.','oh','!','apple','pen']))
+    print(vocab)
+    id2token,token2id  = getLookupTables(vocab)
+    res = idf_t(documents,token2id)
+    print(res)
+    print('- '*50)
+def test_tfidf():
+    vocab = list(set(['i','have','a','pen','.','i','have','an','apple','.','oh','!','apple','pen']))
+    id2token,token2id  = getLookupTables(vocab) 
+    strategy = ScoreAlgorithm(token2id)
+    documents = [['i','have','a','pen','.','a'],
+                ['i','have','an','apple','.'],
+                ['oh','!','apple','pen']]
+    tfidf_scores = strategy.calculate(documents)
+    print(tfidf_scores)
+    assert  tfidf_scores[0][token2id['a']]==0.686512104608772
 test_tf()
+test_idf()
+test_tfidf()
+#test_idf()
